a21c7fea4ec93dfa8f847171947aeec3fbfd223e
tommytnez <tommytnez@yahoo.com>
2017-10-03 19:46:41 -0700

    find_average

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function tabbedContent (evt, tab) {
+function tabbedContent(evt, tab) {
   var tabContent = document.getElementsByClassName('tabContent');
   var tabLinks = document.getElementsByClassName('tabLinks');
 
@@ -9,3 +9,15 @@ function tabbedContent (evt, tab) {
   document.getElementById(tab).style.display = 'block';
   evt.currentTarget.className += ' active';
 }
+
+function find_average(array) {
+  newArr = 0;
+  count = 0;
+  for (var i = 0; i < array.length; i++) {
+    newArr += array[i];
+    count++;
+  }
+  return newArr / count;
+}
+
+console.log(find_average([1, 2, 3]));
